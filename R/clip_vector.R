dde510ceb987cb50efa9179c3554683b6c8f855e
josesamos <jsamos@ugr.es>
2023-10-02 11:34:50 +0200

    Initial commit

---
+++ R/clip_vector.R
@@ -0,0 +1,20 @@
+#' Clip a vector layer based on a polygon
+#'
+#' We must ensure that the layer to be cut has the correct encoding.
+#'
+#' @param vector A `sf` vector layer.
+#' @param polygon A `sf` polygon layer.
+#'
+#' @return A `sf` vector layer.
+#'
+#' @family transformation functions
+#'
+#' @examples
+#'
+#'
+#' @export
+clip_vector <- function(vector, polygon) {
+  s <- sf::st_transform(polygon, sf::st_crs(vector))
+  r <- sf::st_intersection(vector, s)
+  sf::st_transform(r, sf::st_crs(polygon))
+}
