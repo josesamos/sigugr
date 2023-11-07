2d2884f3bcdfad54c70632893b1b057fe66575b0
Jose Samos <jsamos@ugr.es>
2023-11-07 12:33:47 +0100

    pg_write_raster_file_bands function

---
+++ R/pg_write_raster_file_bands.R
@@ -0,0 +1,31 @@
+#' Save raster file bands in separate tables in PostGIS
+#'
+#' @param file A string vector, raster file names.
+#' @param conn A connection to a PostGIS database.
+#' @param prefix A string, table name prefix to the band name.
+#' @param postfix A string, table name postfix to the band name.
+#'
+#' @return A string vector, raster file names.
+#'
+#' @family save functions
+#'
+#' @examples
+#' #
+#'
+#' @export
+pg_write_raster_file_bands <-
+  function(file,
+           conn,
+           prefix = NULL,
+           postfix = NULL) {
+    for (f in file) {
+      name <- basename(f)
+      layer <- gsub(".tif", "", name, ignore.case = TRUE)
+      r <- terra::rast(f)
+      pg_write_bands(r,
+                     conn,
+                     prefix = paste0(prefix, name, '_'),
+                     postfix = postfix)
+    }
+    file
+  }
